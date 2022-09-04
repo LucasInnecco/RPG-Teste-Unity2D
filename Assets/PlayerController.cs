9e9b92d9bf1c08a2c6cc1d14eceab29a230cd7fa
LucasInn <lucas.s.innecco@gmail.com>
2022-09-04 19:46:24 -0400

    fix movimento p/ deslizar nas diagonais ao colidir

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -24,17 +24,32 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate(){
         // If movement input is not 0, try to move
         if(movementInput != Vector2.zero){
-            // Check for potential collisions
+            bool success = TryMove(movementInput);
+            
+            if(!success){
+                success = TryMove(new Vector2(movementInput.x, 0));
+
+                if(!success){
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+            }
+        }
+    }
+
+    private bool TryMove(Vector2 direction){
+        // Check for potential collisions
             int count = rb.Cast(
-                movementInput, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+                direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
                 movementFilter,  // The settings that determine where a collision can  occur on such as layers to collide with
                 castCollisions, // List of collisions to store the found collisions into after the Cast is finished
                 moveSpeed * Time.fixedDeltaTime + collisionOffset // The amount to cast equal to the movement plus an offset
             );
             if(count == 0){
-                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            }else{
+                return false;
             }
-        }
     }
 
     void OnMove(InputValue movementValue){
