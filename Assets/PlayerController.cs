1fbe081790a378b9afa6de635fdd348ad0eff673
LucasInn <lucas.s.innecco@gmail.com>
2022-09-04 19:37:58 -0400

    ajustado script e colisão de movimento do player

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float moveSpeed = 1f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+
+    Vector2 movementInput;
+    Rigidbody2D rb;
+    List<RaycastHit2D> castCollisions =  new List<RaycastHit2D>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void FixedUpdate(){
+        // If movement input is not 0, try to move
+        if(movementInput != Vector2.zero){
+            // Check for potential collisions
+            int count = rb.Cast(
+                movementInput, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+                movementFilter,  // The settings that determine where a collision can  occur on such as layers to collide with
+                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
+                moveSpeed * Time.fixedDeltaTime + collisionOffset // The amount to cast equal to the movement plus an offset
+            );
+            if(count == 0){
+                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+            }
+        }
+    }
+
+    void OnMove(InputValue movementValue){
+        movementInput = movementValue.Get<Vector2>();
     }
 }
