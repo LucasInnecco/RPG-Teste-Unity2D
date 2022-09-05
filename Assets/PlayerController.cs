01d9dbd9fa406b21d04684151df716a7fe777212
LucasInn <lucas.s.innecco@gmail.com>
2022-09-04 20:41:44 -0400

    add animações de idle e andando

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -12,13 +12,17 @@ public class PlayerController : MonoBehaviour
     public ContactFilter2D movementFilter;
 
     Vector2 movementInput;
+    SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
+    Animator animator;
     List<RaycastHit2D> castCollisions =  new List<RaycastHit2D>();
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate(){
@@ -28,16 +32,30 @@ public class PlayerController : MonoBehaviour
             
             if(!success){
                 success = TryMove(new Vector2(movementInput.x, 0));
-
-                if(!success){
-                    success = TryMove(new Vector2(0, movementInput.y));
-                }
             }
+            if(!success){
+                success = TryMove(new Vector2(0, movementInput.y));
+            }
+
+            animator.SetBool("isMoving", success);
+        }else{
+            animator.SetBool("isMoving", false);
         }
+
+        print("isMoving: " + animator.GetBool("isMoving"));
+
+        // Set direction of sprite to movement direction
+        if(movementInput.x < 0){
+            spriteRenderer.flipX = true;
+        }else if (movementInput.x > 0){
+            spriteRenderer.flipX = false;
+        }
+        
     }
 
     private bool TryMove(Vector2 direction){
-        // Check for potential collisions
+        if(direction != Vector2.zero){
+            // Check for potential collisions
             int count = rb.Cast(
                 direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
                 movementFilter,  // The settings that determine where a collision can  occur on such as layers to collide with
@@ -50,6 +68,11 @@ public class PlayerController : MonoBehaviour
             }else{
                 return false;
             }
+        }else{
+            // Can't move if there's no direction to move in
+            return false;
+        }
+
     }
 
     void OnMove(InputValue movementValue){
